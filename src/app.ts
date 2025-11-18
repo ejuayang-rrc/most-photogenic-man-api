e0e20858ec60849d1e7f2b90cabfd594157f1e2d
ejuayang-rrc <ejuayang@rrc.ca>
2025-11-17 19:50:50 -0600

    Added multer configuration.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express, { Express } from "express";
 
+import imageRoutes from "../src/api/v1/routes/imageRoutes";
+
+// Get environment variables
+require("dotenv").config();
+
 // Initialize Express application
 const app: Express = express();
 
 // Ensures incoming body is correctly parsed to JSON
 app.use(express.json());
+
+// Serve uploaded files
+app.use("/uploads", express.static("uploads"));
 
 /**
  * Health check route that returns server status details
@@ -18,3 +26,7 @@ app.get("/api/v1/health", (req, res) => {
         version: "1.0.0",
     });
 });
+
+app.use("/api/v1/image", imageRoutes)
+
+export default app;
---
+++ src/config/multerConfig.ts
@@ -0,0 +1,44 @@
+import multer, { 
+    FileFilterCallback, 
+    Multer, 
+    StorageEngine 
+} from "multer";
+import { Request } from "express";
+
+// Handles file storing
+const storage: StorageEngine = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, "uploads");
+    },
+    filename: (req, file, callback) => {
+        callback(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+
+/**
+ * Filters uploaded files to only allow jpeg and png formats
+ * @param req The Express `Request` object
+ * @param file Object containing information about the processed file
+ * @param callback Function to control which files should be uploaded and which should be skipped
+ */
+const fileFilter = (
+    req: Request, 
+    file: Express.Multer.File, 
+    callback: FileFilterCallback
+): void => {
+    const allowedTypes = ["image/jpeg", "image/png"];
+    
+    if (!allowedTypes.includes(file.mimetype)) {
+        callback(new Error("Only images are allowed"));
+    }
+
+    callback(null, true);
+};
+
+const upload: Multer = multer({
+    storage,
+    limits: { fileSize: 5 * 1024 * 1024 }, 
+    fileFilter,
+});
+
+export default upload;
